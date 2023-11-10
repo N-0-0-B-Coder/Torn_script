6df4b0020ce84e72708695e773994dcbf14c69fd
Dao Chau Nghia <daochaunghia94@gmail.com>
2023-11-10 14:08:59 +0700

    fix bugs

--- Employee Addiction.user.js
+++ Employee Addiction.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Employee Addiction
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @updateURL    https://github.com/N-0-0-B-Coder/Torn_script/raw/main/Employee%20Addiction.user.js
 // @downloadURL  https://github.com/N-0-0-B-Coder/Torn_script/raw/main/Employee%20Addiction.user.js
 // @description  Display employee addiction values and message them with text when click on name
@@ -22,6 +22,7 @@
 
         if (!response.ok) {
             throw new Error('Incorrect key');
+            throw new Error('Access level of this key is not high enough');
         }
 
         return response.json();
