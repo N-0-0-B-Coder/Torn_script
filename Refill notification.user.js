49a9a874a84aee24c50bd3214a55d3389991af8d
Dao Chau Nghia <daochaunghia94@gmail.com>
2023-11-11 21:47:50 +0700

    fix bugs

--- Refill notification.user.js
+++ Refill notification.user.js
@@ -1,18 +1,128 @@
-// ==UserScript==
-// @name         Refill notification
+/// ==UserScript==
+// @name         Refill Notification
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Notify user about 3 daily refill by 3 dots under user points
+// @description  Notify user about daily refills with dots under user points
 // @author       DaoChauNghia [3029549]
 // @match        https://www.torn.com/*
 // @exclude      https://www.torn.com/preferences*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
-// @grant        GM_setValue
-// @grant        GM_getValue
+// @grant        none
 // ==/UserScript==
 
-(function() {
+(async function () {
     'use strict';
 
-    // Your code here...
+    var controls = {}; // Object to store controls
+
+
+    //function to save switch state
+    function saverefillswitch() {
+        localStorage.refillnoti = JSON.stringify(controls);
+    }
+
+    //function to load switch state
+    function loadrefillswitch() {
+        const storedControls = JSON.parse(localStorage.refillnoti || '{}');
+        controls.refillswitch = storedControls.refillswitch || false;
+    }
+
+    // Verify the stored API key
+    let apiKey = localStorage.getItem('tornApiKey');
+    if (!apiKey || apiKey === 'null') {
+        apiKey = prompt('Please enter your Torn API key:');
+        if (!apiKey) {
+            // User canceled the prompt, set API key to "null"
+            apiKey = 'null';
+        }
+    }
+
+    const verificationUrl = `https://api.torn.com/user/?selections=refills&key=${apiKey}`;
+
+    try {
+        const response = await fetch(verificationUrl);
+        var data = await response.json();
+
+        if (data.error && data.error.code === 2) {
+            // Incorrect key, alert the user, set API key to "null", and reload the page
+            alert('Incorrect API key. Please enter a valid Torn API key.');
+            localStorage.setItem('tornApiKey', 'null');
+            location.reload();
+        } else {
+            // API key is valid, store it
+            localStorage.setItem('tornApiKey', apiKey);
+
+            // Load the switch state before creating the switch button and dots
+            loadrefillswitch();
+
+            // Create a box as a child element of the specified class element
+            const pointBlock = document.querySelector('.point-block___rQyUK.tt-points-value');
+            const refillBox = document.createElement('div');
+            refillBox.style.display = 'flex';
+            refillBox.style.alignItems = 'center';
+
+            // Create dots with characters for energy, nerve, and token refills
+            const energyDot = createDot('#69a829', !data.refills.energy_refill_used, 'E');
+            const nerveDot = createDot('#c66231', !data.refills.nerve_refill_used, 'N');
+            const tokenDot = createDot('purple', !data.refills.token_refill_used, 'T');
+
+            // Append dots to the refillBox
+            refillBox.appendChild(energyDot);
+            refillBox.appendChild(nerveDot);
+            refillBox.appendChild(tokenDot);
+
+            // Append the refillBox to the pointBlock
+            pointBlock.appendChild(refillBox);
+
+            // Create a switch button
+            const switchButton = createSwitchButton(refillBox);
+            refillBox.appendChild(switchButton);
+        }
+    } catch (error) {
+        console.error('Error verifying API key:', error);
+        alert('Error verifying API key. Please try again.');
+        localStorage.setItem('tornApiKey', 'null'); // Reset API key to "null" in case of an error
+        location.reload();
+    }
+
+    // Function to create colored dot with character
+    function createDot(color, condition, character) {
+        const dot = document.createElement('div');
+        dot.style.width = '10px';
+        dot.style.height = '10px';
+        dot.style.borderRadius = '50%';
+        dot.style.marginRight = '5px';
+        dot.style.backgroundColor = condition ? color : 'transparent';
+        dot.style.color = condition ? 'white' : 'transparent';
+        dot.style.display = 'flex';
+        dot.style.alignItems = 'center';
+        dot.style.justifyContent = 'center';
+        dot.innerText = character;
+        return dot;
+    }
+
+    // Function to create switch button
+    function createSwitchButton(refillBox) {
+        const switchButton = document.createElement('a');
+        switchButton.textContent = '[switch]';
+        switchButton.className = 't-blue'; // Add "t-blue" class to the button
+        switchButton.style.display = 'flex';
+        switchButton.style.marginLeft = 'auto'; // Add the dots at right of dots
+        switchButton.style.cursor = 'pointer';
+        switchButton.addEventListener('click', () => {
+            controls.refillswitch = !controls.refillswitch;
+            // Toggle the visibility of characters in the dots
+            const dots = refillBox.querySelectorAll('div');
+            dots.forEach(dot => {
+                if (controls.refillswitch) {
+                    dot.style.color = 'white';
+                } else {
+                    dot.style.color = 'transparent';
+                }
+            });
+            saverefillswitch();
+        });
+        return switchButton;
+    }
+
+
 })();
