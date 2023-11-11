f7217c525f3d718d50282f967e566b080928bdef
Dao Chau Nghia <daochaunghia94@gmail.com>
2023-11-11 15:40:49 +0700

    file created

---
+++ Refill notification.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Refill notification
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Notify user about 3 daily refill by 3 dots under user points
+// @author       DaoChauNghia [3029549]
+// @match        https://www.torn.com/*
+// @exclude      https://www.torn.com/preferences*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
