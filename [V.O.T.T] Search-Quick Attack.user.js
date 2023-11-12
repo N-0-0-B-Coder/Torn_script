53b85d38420c56c6b556ae9e3f0cf06125cf9e61
N-0-0-B-Coder <91017748+N-0-0-B-Coder@users.noreply.github.com>
2023-11-12 17:11:15 +0700

    Update [V.O.T.T] Search-Quick Attack.user.js

--- [V.O.T.T] Search-Quick Attack.user.js
+++ [V.O.T.T] Search-Quick Attack.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Advance Search-Quick Attack
+// @name         [V.O.T.T] Search-Quick Attack
 // @namespace    http://tampermonkey.net/
 // @version      1.0.1
 // @updateURL    https://github.com/N-0-0-B-Coder/Torn_script/raw/main/%5BV.O.T.T%5D%20Search-Quick%20Attack.user.js
