1323729db9e1a29bc9a6334c1b949cf033897db0
Dao Chau Nghia <daochaunghia94@gmail.com>
2023-11-19 21:40:09 +0700

    fix bugs

---
+++ [V.O.T.T] Employee Effectiveness.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         [V.O.T.T] Employee Effectiveness
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Calculate the effectiveness of employee based on their stats and position required stats.
+// @author       DaoChauNghia [3029549]
+// @match        https://www.torn.com/*php*
+// @exclude      https://www.torn.com/preferences*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+})();
