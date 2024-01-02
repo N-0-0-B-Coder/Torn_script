02881005e0ec95b854ef39a3efd0a378e81a51fe
Dao Chau Nghia <daochaunghia94@gmail.com>
2024-01-02 22:15:32 +0700

    fix bugs

--- [V.O.T.T] Quick Trade Button for YFT (Adv).user.js
+++ [V.O.T.T] Quick Trade Button for YFT (Adv).user.js
@@ -14,8 +14,8 @@
     'use strict';
 
     const controls = {};
-    controls.traderlist = [];
-    controls.traderID = {};
+    controls.YFTlist = [];
+    controls.YFTId = {};
 
     //#region Ultilities
 
@@ -55,8 +55,8 @@
     // Function to load state using GM_getValue
     function GM_LoadData() {
         const storedControls = JSON.parse(GM_getValue('State', '{}'));
-        controls.traderID = storedControls.traderID || {};
-        controls.traderlist = storedControls.traderlist || [];
+        controls.YFTId = storedControls.YFTId || {};
+        controls.YFTlist = storedControls.YFTlist || [];
     }
 
     //#endregion
@@ -70,9 +70,9 @@
         const tradeButton = document.createElement("button");
         tradeButton.type = "button";
         tradeButton.classList.add("chat-box-options__button___O1B8v");
-        tradeButton.id = "TradeButton" + controls.traderID[name];
+        tradeButton.id = "TradeButton" + controls.YFTId[name];
         tradeButton.addEventListener("click", function () {
-            window.open("https://www.torn.com/trade.php#step=start&userID=" + controls.traderID[name]);
+            window.open("https://www.torn.com/trade.php#step=start&userID=" + controls.YFTId[name]);
         });
 
         const tradeButtonDiv = document.createElement("div");
@@ -99,7 +99,7 @@
 
     function addTradeButtonToChat(chatElement) {
         const chatName = chatElement.parentNode.querySelector("div.chat-box-header___P15jw > button > p").innerText;
-        if (controls.traderlist.includes(chatName) && !chatElement.querySelector("#TradeButton" + controls.traderID[chatName])) {
+        if (controls.YFTlist.includes(chatName) && !chatElement.querySelector("#TradeButton" + controls.YFTId[chatName])) {
             const tradeButton = createTradeButton(chatName);
             const chatinfo = chatElement.querySelector("div:nth-child(1) > button:nth-child(1)");
             chatinfo.after(tradeButton);
@@ -139,18 +139,18 @@
         favoriteTrader.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="default___XXAGt profileButtonIcon" filter="" fill="yellow" stroke="#d4d4d4" stroke-width="0" width="46" height="46" viewBox="694.6 178 46 46"><path d="M 708.5246 202.2194 L 699.8984 194.9941 L 711.2526 194.1093 L 715 183 L 719.7313 194.1093 L 731.0117 194.9941 L 722.3854 202.2194 L 725.1871 213.2786 L 715.455 207.2329 L 705.7966 213.5735 L 708.5246 202.2194 m 6.4754 -6.2194 H 718 V 195 H 714 V 206 L 715 206 L 715 200 L 718 200 V 199 H 715 M 708 195 L 709 200 L 710 202 L 710 200 L 709 195 M 713 195 L 712 195 L 711 200 L 711 202 L 712 200 M 710 201 L 711 201 L 711 200 L 710 200 M 711 201 L 710 201 L 710 206 L 711 206 M 719 196 L 724 196 L 724 195 L 719 195 M 721 196 L 721 206 L 722 206 l 0 -10 Z"></path></svg>';
 
         favoriteTrader.addEventListener("click", function () {
-            if (controls.traderlist.includes(profileName)) {
-                controls.traderlist.splice(controls.traderlist.indexOf(profileName), 1);
-                delete controls.traderID[profileName];
+            if (controls.YFTlist.includes(profileName)) {
+                controls.YFTlist.splice(controls.YFTlist.indexOf(profileName), 1);
+                delete controls.YFTId[profileName];
                 favoriteTrader.classList.remove("active");
-                console.log(controls.traderlist, controls.traderID);
+                console.log(controls.YFTlist, controls.YFTId);
                 GM_SaveData(controls);
                 NotificationYFT("Removed " + profileName + " from your YFT list.");
             } else {
-                controls.traderlist.push(profileName);
-                controls.traderID[profileName] = profileID;
+                controls.YFTlist.push(profileName);
+                controls.YFTId[profileName] = profileID;
                 favoriteTrader.classList.add("active");
-                console.log(controls.traderlist, controls.traderID);
+                console.log(controls.YFTlist, controls.YFTId);
                 GM_SaveData(controls);
                 NotificationYFT("Added " + profileName + " to your YFT list.");
             }
