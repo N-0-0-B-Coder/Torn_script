0bfa42a8f331184663dbe05a96742dc10eea0163
Dao Chau Nghia <daochaunghia94@gmail.com>
2024-03-29 23:41:15 +0700

    first upload

---
+++ Spies Export.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Spies Export
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-29
+// @description  try to take over the world!
+// @author       DaoChauNghia
+// @match        https://www.tornstats.com/spies/faction?page=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tornstats.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+})();
