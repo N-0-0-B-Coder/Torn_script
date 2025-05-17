ef302dfd22b80df4c2e78bfd91e44db4778cbdc9
Đào Châu Nghĩa <daochaunghia94@gmail.com>
2025-05-17 20:32:28 +0700

    Update Total WorkStat-Advanced Search.user.js

--- Total WorkStat-Advanced Search.user.js
+++ Total WorkStat-Advanced Search.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Total WorkStat-Advanced Search
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @updateURL    https://github.com/N-0-0-B-Coder/Torn_script/raw/main/Total%20WorkStat-Advanced%20Search.user.js
 // @downloadURL  https://github.com/N-0-0-B-Coder/Torn_script/raw/main/Total%20WorkStat-Advanced%20Search.user.js
 // @description  Present workstat value beside player name in advanced search
 // @author       DaoChauNghia[3029549]
 // @match        https://www.torn.com/page.php?sid=UserList*
+// @match        https://www.torn.com/profiles.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.j
 // @grant        GM_addStyle
@@ -30,10 +31,47 @@ let GM_addStyle = function (s) {
 }
 `);
 
-    const observerTarget = $(".content-wrapper")[0];
-    const observerConfig = { attributes: false, childList: true, characterData: false, subtree: true };
-    const unavailable = ['Job','Federal'];
-    const workstatCache = {};
+    // #region WorkStat Filter Switch
+
+    var controls = {}; // Initialize an empty dictionary to store controls
+
+    // Function to save switch state
+    function swSaveFilterSwitch() {
+        GM_setValue('WSFFilter', JSON.stringify(controls));
+    }
+
+    // Function to load switch state
+    function swLoadFilterSwitch() {
+        const storedControls = JSON.parse(GM_getValue('WSFFilter', '{}'));
+        controls.StackEnabled = storedControls.StackEnabled || false;
+    }
+
+    // Function to create switch button
+    function createSwitchButton() {
+        const switchButton = document.createElement('a');
+        if (controls.StackEnabled) {
+            switchButton.textContent = '[WorkStatFilter enabled]';
+        } else {
+            switchButton.textContent = '[WorkStatFilter disabled]';
+        }
+        switchButton.className = 't-blue'; // Add "t-blue" class to the button
+        switchButton.style.display = 'flex';
+        switchButton.style.marginLeft = 'auto'; // Add the dots at the right of dots
+        switchButton.style.cursor = 'pointer';
+        switchButton.addEventListener('click', () => {
+            controls.StackEnabled = !controls.StackEnabled;
+
+            if (controls.StackEnabled) {
+                switchButton.textContent = '[WorkStatFilter enabled]';
+            } else {
+                switchButton.textContent = '[WorkStatFilter disabled]';
+            }
+            swSaveFilterSwitch();
+        });
+        return switchButton;
+    }
+
+    // #endregion WorkStat Filter Switch
 
     // #region API Key Validation
 
@@ -97,89 +135,137 @@ let GM_addStyle = function (s) {
 
     // #endregion API Key Validation
 
-    let AdSearchobserver = new MutationObserver(function(mutations) {
-        mutations.forEach((mutation) => {
-            if (
-                mutation.target.classList.contains("user-info-list-wrap") ||
-                mutation.target.classList.contains("userlist-wrapper")
-            ) {
-                let containerID = $("ul.user-info-list-wrap > li");
-                containerID.each(function () {
-                    let status = this.querySelectorAll('ul#iconTray > li');
-                    // Check if any status is unavailable
-                    let isUnavailable = false;
-                    for (let s of status) {
-                        if (unavailable.some(u => s.title.includes(u))) {
-                            this.style.display = 'none';
-                            isUnavailable = true;
-                            break;
+    swLoadFilterSwitch(); // Load the switch state
+
+    // Function to add switch button to the page
+    const SWFilterBar = document.querySelector('.bar___Bv5Ho').parentNode.insertBefore(createSwitchButton(), document.querySelectorAll('.bar___Bv5Ho')[0]);
+
+    // Only run the script on the specific page
+    if (window.location.pathname === '/page.php' && window.location.search.startsWith('?sid=UserList')) {
+        const observerTarget = $(".content-wrapper")[0];
+        const observerConfig = { attributes: false, childList: true, characterData: false, subtree: true };
+        var unavailable = [];
+        if (controls.StackEnabled) {
+            unavailable = ['Job','Federal'];
+        } else {
+            unavailable = ['Federal'];
+        }
+        const workstatCache = {};
+
+
+        let AdSearchobserver = new MutationObserver(function(mutations) {
+            mutations.forEach((mutation) => {
+                if (
+                    mutation.target.classList.contains("user-info-list-wrap") ||
+                    mutation.target.classList.contains("userlist-wrapper")
+                ) {
+                    let containerID = $("ul.user-info-list-wrap > li");
+                    containerID.each(function () {
+                        let status = this.querySelectorAll('ul#iconTray > li');
+                        // Check if any status is unavailable
+                        let isUnavailable = false;
+                        for (let s of status) {
+                            if (unavailable.some(u => s.title.includes(u))) {
+                                this.style.display = 'none';
+                                isUnavailable = true;
+                                break;
+                            }
                         }
-                    }
-                    if (isUnavailable) return; // Skip further processing for this user
+                        if (isUnavailable) return; // Skip further processing for this user
 
-                    let user = this.className;
-                    let userID = user.replace("user", "");
-                    let userIcons = $(this).find("div.level-icons-wrap > span.user-icons");
-                    if (
-                        userIcons.length > 0 &&
-                        !userIcons[0].hasAttribute('data-ws-processed')
-                    ) {
-                        userIcons[0].setAttribute('data-ws-processed', 'true');
-                        // Remove any existing block before adding a new one
-                        let existing = userIcons[0].querySelector('.advance-search-workstat');
-                        if (existing) existing.remove();
-                        insertWorkstat(userIcons[0], userID);
-                    }
-                    let iconWrap = this.querySelector('span.icons-wrap');
-                    iconWrap.style.display = 'inline';
-                    let iconTray = this.querySelector('span.icons-wrap > ul#iconTray');
-                    iconTray.style.display = 'inline';
-                });
-            }
+                        let user = this.className;
+                        let userID = user.replace("user", "");
+                        let userIcons = $(this).find("div.level-icons-wrap > span.user-icons");
+                        if (
+                            userIcons.length > 0 &&
+                            !userIcons[0].hasAttribute('data-ws-processed')
+                        ) {
+                            userIcons[0].setAttribute('data-ws-processed', 'true');
+                            // Remove any existing block before adding a new one
+                            let existing = userIcons[0].querySelector('.advance-search-workstat');
+                            if (existing) existing.remove();
+                            insertWorkstat(userIcons[0], userID);
+                        }
+                        let iconWrap = this.querySelector('span.icons-wrap');
+                        iconWrap.style.display = 'inline';
+                        let iconTray = this.querySelector('span.icons-wrap > ul#iconTray');
+                        iconTray.style.display = 'inline';
+                    });
+                }
+            });
         });
-    });
 
-    AdSearchobserver.observe(observerTarget, observerConfig);
+        AdSearchobserver.observe(observerTarget, observerConfig);
 
-    function insertWorkstat(element, ID){
-        // If we already have the value, use it
-        if (workstatCache[ID] !== undefined) {
-            addWSBlock(element, workstatCache[ID]);
-            return;
+        function insertWorkstat(element, ID){
+            // If we already have the value, use it
+            if (workstatCache[ID] !== undefined) {
+                addWSBlock(element, workstatCache[ID]);
+                return;
+            }
+
+            // Otherwise, fetch and cache
+            fetch(`https://api.torn.com/v2/user/${ID}/hof?key=${storedApiKey}`)
+                .then(response => response.json())
+                .then(data => {
+                    let value = (data && data.hof && data.hof.working_stats) ? data.hof.working_stats.value : 'N/A';
+                    workstatCache[ID] = value;
+                    addWSBlock(element, value);
+                })
+                .catch(error => {
+                    addWSBlock(element, 'N/A');
+                });
         }
 
-        // Otherwise, fetch and cache
-        fetch(`https://api.torn.com/v2/user/${ID}/hof?key=${storedApiKey}`)
-            .then(response => response.json())
-            .then(data => {
-                let value = (data && data.hof && data.hof.working_stats) ? data.hof.working_stats.value : 'N/A';
-                workstatCache[ID] = value;
-                addWSBlock(element, value);
-            })
-            .catch(error => {
-                addWSBlock(element, 'N/A');
-            });
+        // Separate function to add the block
+        function addWSBlock(element, value) {
+            const outerspanatk = document.createElement('span');
+            outerspanatk.className = 'bold btn-wrap advance-search-workstat';
+
+            const wsBlock = document.createElement('div');
+            wsBlock.textContent = value;
+            wsBlock.style.padding = '6px 16px';
+            wsBlock.style.background = 'transparent';
+            wsBlock.style.color = '#fff';
+            wsBlock.style.borderRadius = '4px';
+            wsBlock.style.display = 'inline-block';
+            wsBlock.style.cursor = 'default';
+            wsBlock.style.fontWeight = 'bold';
+            wsBlock.style.fontSize = '16px';
+            wsBlock.style.width = 'auto';
+
+            outerspanatk.appendChild(wsBlock);
+            element.append(outerspanatk);
+        }
     }
 
-    // Separate function to add the block
-    function addWSBlock(element, value) {
-        const outerspanatk = document.createElement('span');
-        outerspanatk.className = 'bold btn-wrap advance-search-workstat';
+    if (window.location.pathname === '/profiles.php') {
 
-        const wsBlock = document.createElement('div');
-        wsBlock.textContent = value;
-        wsBlock.style.padding = '6px 16px';
-        wsBlock.style.background = 'transparent';
-        wsBlock.style.color = '#fff';
-        wsBlock.style.borderRadius = '4px';
-        wsBlock.style.display = 'inline-block';
-        wsBlock.style.cursor = 'default';
-        wsBlock.style.fontWeight = 'bold';
-        wsBlock.style.fontSize = '16px';
-        wsBlock.style.width = 'auto';
+        let profileRoot = document.querySelector('.core-layout__viewport > .user-profile');
+        let ProfileWrapper = profileRoot.getElementsByClassName('profile-wrapper')[2]; // get the third element
+        let BasicInfo = ProfileWrapper.getElementsByClassName('info-table')[0];
+        let UserInfo = BasicInfo.childNodes[1];
+        let UserInfoTable = UserInfo.childNodes[1];
 
-        outerspanatk.appendChild(wsBlock);
-        element.append(outerspanatk);
+        let UserWorkStat = document.createElement('span');
+
+
+        let userID = window.location.search.replace("?XID=", "");
+        fetch(`https://api.torn.com/v2/user/${userID}/hof?key=${storedApiKey}`)
+            .then(response => response.json())
+            .then(data => {
+            let value = (data && data.hof && data.hof.working_stats) ? data.hof.working_stats.value : 'N/A';
+            if (typeof value === 'number') {
+                value = value.toLocaleString();
+            }
+
+            UserWorkStat.innerHTML = " (WorkStat: " + value + ")";
+            UserInfoTable.appendChild(UserWorkStat);
+            })
+            .catch(error => {
+            UserWorkStat.innerHTML = " (WorkStat: ERROR)";
+            UserInfoTable.appendChild(UserWorkStat);
+            });
     }
 
 })();
